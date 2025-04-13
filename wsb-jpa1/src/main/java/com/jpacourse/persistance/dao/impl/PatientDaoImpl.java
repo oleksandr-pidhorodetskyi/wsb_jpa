8b47abce74f6c7511c49c36ead268d575fbd33b0
APidhorodetskyi <s.pidgorodetsky@gmail.com>
2025-04-13 10:18:40 +0200

    fix lab2 patientDaoImpl

--- wsb-jpa1/src/main/java/com/jpacourse/persistance/dao/impl/PatientDaoImpl.java
+++ wsb-jpa1/src/main/java/com/jpacourse/persistance/dao/impl/PatientDaoImpl.java
@@ -1,11 +1,13 @@
 package com.jpacourse.persistance.dao.impl;
 
+import com.jpacourse.persistance.dao.DoctorDao;
 import com.jpacourse.persistance.dao.PatientDao;
 import com.jpacourse.persistance.entity.DoctorEntity;
 import com.jpacourse.persistance.entity.PatientEntity;
 import com.jpacourse.persistance.entity.VisitEntity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -14,13 +16,14 @@ import java.util.ArrayList;
 @Repository
 public class PatientDaoImpl extends AbstractDao<PatientEntity, Long> implements PatientDao {
 
-    @PersistenceContext
-    private EntityManager em;
+
+@Autowired
+private DoctorDao doctorDao;
 
     @Override
     public void addVisitToPatient(Long patientId, Long doctorId, LocalDateTime time, String description) {
-        PatientEntity patient = em.find(PatientEntity.class, patientId);
-        DoctorEntity doctor = em.find(DoctorEntity.class, doctorId);
+        PatientEntity patient =findOne(patientId);
+        DoctorEntity doctor = doctorDao.findOne(doctorId);
 
         if (patient == null || doctor == null) {
             throw new IllegalArgumentException("Patient or doctor not found");
@@ -39,6 +42,6 @@ public class PatientDaoImpl extends AbstractDao<PatientEntity, Long> implements 
         patient.getVisits().add(visit);
 
         // Kaskadowy zapis wizyty
-        em.merge(patient);
+        update(patient);
     }
 }
